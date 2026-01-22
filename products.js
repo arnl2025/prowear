8548f53ae403a41fe97fb03a7ced48e424386b71
AL <146753285+arnl2025@users.noreply.github.com>
2026-01-22 11:42:11 +0100

    Create products.js

---
+++ products.js
@@ -0,0 +1,20 @@
+const products = [
+  {
+    id: "bollar-001",
+    name: "Golfbollar 8 Pack",
+    price: 115,
+    stock: 14,
+    image: "https://i.postimg.cc/ZnvsDZFh/golfbollar.jpg",
+    qr: "https://i.postimg.cc/02dn8xdn/swish-QR-small-(3).png",
+    description: "Styckepris för 8 stycken golfbollar."
+  },
+  {
+    id: "bollar-002",
+    name: "Golfbollar 12 Pack",
+    price: 175,
+    stock: 12,
+    image: "https://i.postimg.cc/ZnvsDZFh/golfbollar.jpg",
+    qr: "https://i.postimg.cc/T1DBCPSq/swish-QR-small.png",
+    description: "Styckepris för 12 stycken golfbollar."
+  }
+];
