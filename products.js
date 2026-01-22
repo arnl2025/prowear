b165f45650c5f6453b32a7d7010356fbb1a0ca18
AL <146753285+arnl2025@users.noreply.github.com>
2026-01-22 11:51:36 +0100

    Update products.js

--- products.js
+++ products.js
@@ -1,20 +1,11 @@
 const products = [
   {
-    id: "bollar-001",
-    name: "Golfbollar 8 Pack",
-    price: 115,
-    stock: 14,
-    image: "https://i.postimg.cc/ZnvsDZFh/golfbollar.jpg",
-    qr: "https://i.postimg.cc/02dn8xdn/swish-QR-small-(3).png",
-    description: "Styckepris för 8 stycken golfbollar."
-  },
-  {
-    id: "bollar-002",
-    name: "Golfbollar 12 Pack",
-    price: 175,
-    stock: 12,
-    image: "https://i.postimg.cc/ZnvsDZFh/golfbollar.jpg",
-    qr: "https://i.postimg.cc/T1DBCPSq/swish-QR-small.png",
-    description: "Styckepris för 12 stycken golfbollar."
+    id: "nike-001",
+    name: "Nike Tech Fleece",
+    price: 520,
+    stock: 1,
+    image: "https://i.postimg.cc/zfDhD6wj/output-onlinepngtools.png",
+    qr: "https://i.postimg.cc/02dn/sish.png",
+    description: "Nike Tech Fleece"
   }
 ];
